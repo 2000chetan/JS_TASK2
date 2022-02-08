86bdd7546f0a0cd36fdd50fe6d24de56352319eb
Chetan <chetansinghbisht@cedcoss.com>
2022-02-08 16:53:19 +0530

    product table

--- src/index.js
+++ src/index.js
@@ -1,54 +1,38 @@
-const arrProduct = []; //array of products
-
-//   add product function
-function addProduct() {
-    var id = document.getElementById("pid").value;
-    var name = document.getElementById("pname").value;
-    var price = document.getElementById("price").value;
-
-
-
-    // validator for empty fields
-    if (id && name && price) {
-
-        // validator for numeric input
-        if (isNaN(price)) {
-            document.getElementById('error').style.display = "block";
-            document.getElementById('price').style.borderColor = 'red';
-            document.getElementById('error').innerHTML = '*Enter numeric value in price.'
-            return;
-        }
-
-
-        document.getElementById('error').style.display = "none";
-        document.getElementById('price').style.border = '0.5px solid black';
-
-        arrProduct.push({ id: id, name: name, price: price }); //adding elements to array
-        addElement(arrProduct);
-    } else {
-
-        // if text fields are empty
-        document.getElementById('error').style.display = "block";
-        document.getElementById('error').innerHTML = '*Please fill all the details.'
-
-
-    }
+lst = [];
+error_msg = [];
+function fetchData() {
+  let id = document.getElementById("product-id").value;
+  let name = document.getElementById("product-Name").value;
+  let price = document.getElementById("product-Price").value;
+  item = {
+    id: id,
+    name: name,
+    price: price,
+  };
+  if (checkunique(id) == false) {
+    lst.push(item);
+  }
+  return item;
 }
-
-//   add element function to add element in table
-function addElement(arr) {
-    var table =
-        "<table> <tr><th>Product Id</th><th>Product Name</th><th>Price</th></tr> ";
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-        table +=
-            "<tr><td>" +
-            arr[i].id +
-            "</td><td>" +
-            arr[i].name +
-            "</td><td>" +
-            `USD ${arr[i].price}` +
-            "</td></tr>";
+function checkunique(id) {
+  for (let i = 0; i < lst.length; i++) {
+    if (lst[i].id == id) {
+      alert("enter unique id!");
+      return true;
     }
-    document.getElementById("product").innerHTML = table + "</table>";
+  }
+  return false;
+}
+function display() {
+  let table = document.getElementById("tbl");
+  table.innerHTML =
+    "<th>Prodcut id</th> <th>Prodcut Name</th> <th>Prodcut Price </th>";
+  lst.forEach((element) => {
+    table.innerHTML += `<tr><td>${element.id}</td><td>${element.name}</td><td>${element.price}</td></tr>`;
+  });
+}
+function myFunction() {
+  let fetchedData = fetchData();
+  console.log(fetchedData);
+  display();
 }
